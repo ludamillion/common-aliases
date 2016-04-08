9d32bc4751ede0a1f37c30ce97cc73afe78c44cb
Luke Inglis <ld.inglis@gmail.com>
2016-04-08 13:48:24 -0400

    Remove a few redundant or non-useful aliases

--- common-aliases.plugin.zsh
+++ common-aliases.plugin.zsh
@@ -1,6 +1,5 @@
 # Advanced Aliases.
 # Use with caution
-#
 
 # ls, the common ones I use a lot shortened for rapid fire usage
 alias l='ls -lFh'     #size,show type,human readable
@@ -8,6 +7,7 @@ alias la='ls -lAFh'   #long list,show almost all,show type,human readable
 alias lr='ls -tRFh'   #sorted by date,recursive,show type,human readable
 alias lt='ls -ltFh'   #long list,sorted by date,show type,human readable
 alias ll='ls -l'      #long list
+alias l1='ls -1'
 alias ldot='ls -ld .*'
 alias lS='ls -1FSsh'
 alias lart='ls -1Fcart'
@@ -17,8 +17,6 @@ alias zshrc='$EDITOR ~/.zshrc' # Quick access to the ~/.zshrc file
 
 alias grep='grep --color'
 alias sgrep='grep -R -n -H -C 5 --exclude-dir={.git,.svn,CVS} '
-
-alias t='tail -f'
 
 # Command line head / tail shortcuts
 alias -g H='| head'
@@ -50,12 +48,6 @@ alias cd..='cd ..'
 alias gti='git'
 alias gz='tar czvf'
 alias ugz='tar xzvf'
-
-alias ll='ls -l'
-alias la='ls -la'
-alias sl='ls'
-alias docker-rm='docker ps -aq | xargs docker rm'
-alias docker-rmi='docker images -q | xargs docker rmi'
 
 # zsh is able to auto-do some kungfoo
 # depends on the SUFFIX :)
